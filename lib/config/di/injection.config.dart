e6e8aa3b4dfaca0a04db0a47b11fb54c558031c9
Dmitro Serdun <d.serdun@webtrit.com>
2022-12-25 13:03:03 +0200

    Implement get profile use case

--- lib/config/di/injection.config.dart
+++ lib/config/di/injection.config.dart
@@ -10,21 +10,27 @@ import 'package:firebase_auth/firebase_auth.dart' as _i6;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import '../../data/data.dart' as _i11;
+import '../../data/data.dart' as _i12;
 import '../../data/datasource/datasource.dart' as _i5;
 import '../../data/datasource/firebase/firebase_auth_datasource.dart' as _i7;
 import '../../data/datasource/firebase/firebase_realtime_datasource.dart'
     as _i9;
 import '../../data/repository/implementation/auth_repository_impl.dart' as _i4;
-import '../../domain/domain.dart' as _i10;
-import '../../domain/entity/mappers/implementation/profile_mapper.dart' as _i13;
-import '../../domain/entity/mappers/implementation/user_mapper.dart' as _i12;
-import '../../domain/repository/repository.dart' as _i3;
-import '../../domain/usecase/auth/login_credential/login_credential_impl.dart'
+import '../../data/repository/implementation/profile_repository_impl.dart'
     as _i15;
+import '../../domain/domain.dart' as _i10;
+import '../../domain/entity/mappers/implementation/profile_mapper.dart' as _i14;
+import '../../domain/entity/mappers/implementation/user_mapper.dart' as _i13;
+import '../../domain/repository/repository.dart' as _i3;
+import '../../domain/usecase/auth/get_authorized_user/get_authorized_user_impl.dart'
+    as _i16;
+import '../../domain/usecase/auth/is_user_authorized/is_user_authorized_user_impl.dart'
+    as _i11;
+import '../../domain/usecase/auth/login_credential/login_credential_impl.dart'
+    as _i18;
 import '../../domain/usecase/profile/get_profile/get_profile_use_case_impl.dart'
-    as _i14;
-import 'injection.dart' as _i16; // ignore_for_file: unnecessary_lambdas
+    as _i17;
+import 'injection.dart' as _i19; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -47,19 +53,30 @@ _i1.GetIt $initGetIt(
   gh.singleton<_i8.FirebaseFirestore>(registerModule.database());
   gh.singleton<_i9.FirebaseRealtimeDatasource>(
       _i9.FirebaseRealtimeDatasource(get<_i8.FirebaseFirestore>()));
-  gh.factory<_i10.Mapper<_i11.UserDTO, _i10.UserModel>>(
-      () => _i12.UserMapper());
-  gh.factory<_i10.Mapper<_i11.ProfileDTO, _i10.ProfileModel>>(
-      () => _i13.ProfileMapper());
-  gh.factory<_i10.GetProfileUseCase>(() => _i14.GetProfileUseCaseImpl(
+  gh.factory<_i10.IsUserAuthorizedUserUseCase>(
+      () => _i11.IsUserAuthorizedUserUseCaseImpl(get<_i10.AuthRepository>()));
+  gh.factory<_i10.Mapper<_i12.UserDTO, _i10.UserModel>>(
+      () => _i13.UserMapper());
+  gh.factory<_i10.Mapper<_i12.ProfileDTO, _i10.ProfileModel>>(
+      () => _i14.ProfileMapper());
+  gh.factory<_i10.ProfileRepository>(
+      () => _i15.ProfileRepositoryImpl(get<_i5.FirebaseRealtimeDatasource>()));
+  gh.factory<_i10.GetAuthorizedUserUseCase>(
+      () => _i16.GetAuthorizedUserUseCaseImpl(
+            get<_i10.AuthRepository>(),
+            get<_i10.Mapper<_i12.UserDTO, _i10.UserModel>>(),
+          ));
+  gh.factory<_i10.GetProfileUseCase>(() => _i17.GetProfileUseCaseImpl(
         get<_i10.ProfileRepository>(),
-        get<_i10.Mapper<_i11.ProfileDTO, _i10.ProfileModel>>(),
+        get<_i10.IsUserAuthorizedUserUseCase>(),
+        get<_i10.GetAuthorizedUserUseCase>(),
+        get<_i10.Mapper<_i12.ProfileDTO, _i10.ProfileModel>>(),
       ));
-  gh.factory<_i10.LoginCredentialUseCase>(() => _i15.LoginCredentialUseCaseImpl(
+  gh.factory<_i10.LoginCredentialUseCase>(() => _i18.LoginCredentialUseCaseImpl(
         get<_i10.AuthRepository>(),
-        get<_i10.Mapper<_i11.UserDTO, _i10.UserModel>>(),
+        get<_i10.Mapper<_i12.UserDTO, _i10.UserModel>>(),
       ));
   return get;
 }
 
-class _$RegisterModule extends _i16.RegisterModule {}
+class _$RegisterModule extends _i19.RegisterModule {}
--- lib/domain/usecase/profile/get_profile/get_profile_use_case_impl.dart
+++ lib/domain/usecase/profile/get_profile/get_profile_use_case_impl.dart
@@ -4,14 +4,28 @@ import 'package:vinyla/domain/domain.dart';
 
 @Injectable(as: GetProfileUseCase)
 class GetProfileUseCaseImpl implements GetProfileUseCase {
-  GetProfileUseCaseImpl(this.profileRepository, this.mapper);
+  GetProfileUseCaseImpl(
+    this._profileRepository,
+    this._isUserAuthorizedUserUseCase,
+    this._getAuthorizedUserUseCase,
+    this._mapper,
+  );
 
-  final ProfileRepository profileRepository;
-  final Mapper<ProfileDTO, ProfileModel> mapper;
+  final ProfileRepository _profileRepository;
+  final IsUserAuthorizedUserUseCase _isUserAuthorizedUserUseCase;
+  final GetAuthorizedUserUseCase _getAuthorizedUserUseCase;
+  final Mapper<ProfileDTO, ProfileModel> _mapper;
 
   @override
-  Future<ProfileModel> execute(String uuid) async {
-    final dto = await profileRepository.getProfile(uuid);
-    return mapper.mapToModel(dto);
+  Future<ProfileModel> execute() async {
+    final isAuthorized = await _isUserAuthorizedUserUseCase.execute();
+    if (isAuthorized) {
+      final user = await _getAuthorizedUserUseCase.execute();
+      final dto = await _profileRepository.getProfile(user.uuid);
+      return _mapper.mapToModel(dto);
+    } else {
+      // TODO: ADD CUSTOM AUTH EXCEPTION
+      throw Exception("User is not authorized");
+    }
   }
 }
