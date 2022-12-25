f8b9b5b7370163d1219055aaabed5e0b775470b5
Dmitro Serdun <d.serdun@webtrit.com>
2022-12-26 01:32:51 +0200

    Update profile repository

--- lib/data/repository/implementation/profile_repository_impl.dart
+++ lib/data/repository/implementation/profile_repository_impl.dart
@@ -1,17 +1,36 @@
 import 'package:injectable/injectable.dart';
+
 import 'package:vinyla/data/datasource/datasource.dart';
 import 'package:vinyla/data/dto/dto.dart';
+
 import 'package:vinyla/domain/domain.dart';
+
+import '../../datasource/config/profile_initializer.dart';
 
 @Injectable(as: ProfileRepository)
 class ProfileRepositoryImpl implements ProfileRepository {
   final FirebaseDatabaseDatasource firebaseRealtimeDatasource;
+  final ProfileInitializer profileInitializer;
 
-  ProfileRepositoryImpl(this.firebaseRealtimeDatasource);
+  ProfileRepositoryImpl(
+    this.firebaseRealtimeDatasource,
+    this.profileInitializer,
+  );
 
   @override
   Future<ProfileDTO> getProfile(String uid) async {
-    var res = await firebaseRealtimeDatasource.get("/user/$uid");
+    final profilePath = "/user/$uid";
+    final isExits = await firebaseRealtimeDatasource.isExits(profilePath);
+
+    if (isExits) {
+      var res = await firebaseRealtimeDatasource.get(profilePath);
+      return ProfileDTO.fromJson(res);
+    } else {
+      final profile = profileInitializer.generateProfile().toJson();
+      var res = await firebaseRealtimeDatasource.post(profilePath, profile);
+      return ProfileDTO.fromJson(res);
+    }
+  }
 
   @override
   Future<ProfileDTO> createProfile(String uid, ProfileDTO dto) async {
